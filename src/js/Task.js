992e82f66babd5a46b6a548309253a0968234e48
Oleg Kovalenko <kn1feR26rus@yandex.ru>
2021-02-02 19:43:06 +0300

    fix bug progressBar and fonts

--- src/js/Task.js
+++ src/js/Task.js
@@ -11,7 +11,7 @@ export class Task {
         const progressText = document.querySelector('.section__bot_progressBar-text')
         const inputTask = document.querySelector('.section__top_input');
         const submitBtn = document.querySelector('.deleteform__submit');
-    
+        
         //Create elems
         const elem = document.createElement('li');
         const check = document.createElement('input');
@@ -67,6 +67,7 @@ export class Task {
             if(check.checked) {
                 elem.remove();
                 letProgress();
+                progressBar.style.width = 0;
             }
          })
 
@@ -89,6 +90,7 @@ export class Task {
             text.innerText = textarea.value;
             corrBtn.replaceWith(correctBtn);
          })          
+
     }
 
 
